5e66e40f9efa28bc52fbc5d93e945a831dbcfe72
zcxr-gif <hrantony2230@gmail.com>
2025-09-19 20:12:07 -0400

    added the 739

--- server.js
+++ server.js
@@ -601,8 +601,8 @@ const deduceRankFromAircraft = (acStr) => {
     const s = String(acStr || '').toUpperCase();
     if (!s) return 'Unknown';
     const has = (pat) => new RegExp(pat, 'i').test(s);
-    if (has('(Q400|A320|B738)')) return 'IndGo Cadet';
-    if (has('(A321|B737)')) return 'Skyline Observer';
+    if (has('(DH8D|Q400|A320|B738)')) return 'IndGo Cadet';
+    if (has('(A321|B737|B739)')) return 'Skyline Observer';
     if (has('(A330|B38M)')) return 'Route Explorer';
     if (has('(787-8|777-200LR)')) return 'Skyline Officer';
     if (has('(787-9|777-300ER)')) return 'Command Captain';
