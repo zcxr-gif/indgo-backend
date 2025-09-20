1dc368ed87c6d99c302b1d523d2040f1522c7f48
zcxr-gif <hrantony2230@gmail.com>
2025-09-20 17:08:11 -0400

    10mb extended json

--- server.js
+++ server.js
@@ -65,6 +65,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Multer configuration for AWS S3 uploads
