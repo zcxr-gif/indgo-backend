d112f8140a30c484267d7e4b01cdae032bdfc308
zcxr-gif <hrantony2230@gmail.com>
2025-11-24 00:57:37 -0500

    5

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.delete('/api/aircraft/:id', async (req, res) => {
     }
 });
 
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
